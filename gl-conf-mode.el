7ef5023de48e3c63e62ebbcd992ad4d470c2e84e
Luis Lloret <luislloret@gmail.com>
2011-12-15 13:26:25 +0000

    Renamed major mode file to allow loading from emacs

--- gl-conf-mode.el
+++ gl-conf-mode.el
@@ -1,6 +1,6 @@
-;; gitolite-conf-mode.el --- major-mode for editing gitolite config files 
+;; gitolite-conf-mode.el --- major-mode for editing gitolite config files
 ;;
-;; Provides navigation utilities, syntax highlighting and indentation for 
+;; Provides navigation utilities, syntax highlighting and indentation for
 ;; gitolite configuration files (gitolite.conf)
 ;;
 ;; Add this code to your .emacs file to use the mode (and automatically
@@ -14,7 +14,7 @@
 ;; M-x gl-conf-mode, after opening the file
 ;;
 ;; Automatic indentation for this mode is disabled by default.
-;; If you want to enable it, go to Customization menu in emacs, 
+;; If you want to enable it, go to Customization menu in emacs,
 ;; then "Files", "Gitolite Config Files", and select the appropiate option.
 ;;
 ;; The interesting things you can do are:
@@ -30,7 +30,7 @@
 ;; to integrate them in the master repo and make those changes accessible
 ;; to more people
 ;;
-;; Now the MIT license block (this enables you to use this code in a 
+;; Now the MIT license block (this enables you to use this code in a
 ;; very liberal manner)
 ;;
 ;;
@@ -68,24 +68,24 @@
   (when gl-conf-auto-indent-enable
 	(if (bobp)
 		(gl-conf-indent-line-to 0)
-	  (let (cur-indent)	  
+	  (let (cur-indent)
 		(save-excursion
-		  (beginning-of-line)		
+		  (beginning-of-line)
 		  (let ((point-start (point))
 				token)
-			
+
 			;; Search backwards (if there is a repo definition, we indent, otherwise, we don't)
 			(if (re-search-backward gl-conf-regex-repo (point-min) t)
 				(setq cur-indent tab-width) (setq cur-indent 0))
-			
+
 			(goto-char point-start)
-			
+
 			;; Set indentation to zero if this is a repo block
 			(if (looking-at gl-conf-regex-repo)
 				(setq cur-indent 0))))
-		
+
 		(gl-conf-indent-line-to cur-indent))))
-  (unless gl-conf-auto-indent-enable 
+  (unless gl-conf-auto-indent-enable
 	(insert-tab))
   )
 
@@ -122,7 +122,7 @@
   "Moves the cursor to the next repo definition in the current file. Returns t if next repo, nil otherwise."
   (interactive)
   (push-mark)
-  (let ((cur-point (point)))	
+  (let ((cur-point (point)))
 	(end-of-line nil)
 	(if (re-search-forward gl-conf-regex-repo nil t)
 		(progn (beginning-of-line nil) t)
@@ -165,11 +165,11 @@
     (setq endpoint (point))
     (beginning-of-line nil)
     (if (re-search-forward gl-conf-regex-include endpoint t)
-		(find-file (match-string 1) t) 
+		(find-file (match-string 1) t)
 	  (message "Not a include line")))
   )
 
-;; conditional predicate that tells whether a buffer name starts with a * 
+;; conditional predicate that tells whether a buffer name starts with a *
 (defun buffer-starts-with-star (buf)
   (not (string-match "^*" (buffer-name buf)))
   )
@@ -181,10 +181,10 @@
   )
 
 (defun gl-conf-list-repos ()
-  "Opens another window with a list of the repos that were found. It supports hyperlinking, so hitting RET on there will 
-take you to the occurrence. 
+  "Opens another window with a list of the repos that were found. It supports hyperlinking, so hitting RET on there will
+take you to the occurrence.
 
-In recent emacs versions, it will use 'multi-occur', so it navigates through the includes to find references in them as well; 
+In recent emacs versions, it will use 'multi-occur', so it navigates through the includes to find references in them as well;
 Otherwise it will use 'occur', which searches only in the current file."
   (interactive)
   (save-excursion
@@ -203,7 +203,7 @@ Otherwise it will use 'occur', which searches only in the current file."
 
 (defun gl-conf-mark-repo ()
   "Mark (select) the repo where the cursor is."
-  (interactive)  
+  (interactive)
   (let ((cur-point (point)))
 	;; go to previous repo definition line, which is the one that contains the cursor and mark the position
 	(beginning-of-line nil)
@@ -232,12 +232,12 @@ Otherwise it will use 'occur', which searches only in the current file."
 		'("^[ \t]*\\(@[A-Za-z0-9][A-Za-z0-9-_.]+.*\\)" 1 font-lock-warning-face) ;; group wrong definition (warning)
 		'("[= \t][ \t]*\\(@[A-Za-z0-9][A-Za-z0-9-_.]+\\)" 1 font-lock-variable-name-face) ;; group usage
 		'("^[ \t]*\\(?:-\\|R\\|RW\\+?C?D?\\)[ \t]*\\(\\(?:\\w\\|/\\|\\[\\|\\]\\|-\\)+\\)[ \t]*=" 1 font-lock-type-face)) ;; refexes
-  
+
   "gl-conf mode syntax highlighting."
   )
 
 ;;
-;; Syntax table 
+;; Syntax table
 ;;
 (defvar gl-conf-mode-syntax-table nil "Syntax table for gl-conf-mode.")
 (setq gl-conf-mode-syntax-table nil)
@@ -247,7 +247,7 @@ Otherwise it will use 'occur', which searches only in the current file."
 ;;
 (defvar gl-conf-mode-hook nil)
 
-;; 
+;;
 ;; Customization to enable or disable automatic indentation
 ;;
 (defgroup gl-conf nil "Gitolite configuration editing." :tag "Gitolite config files" :prefix "gl-conf" :group 'files)
@@ -296,12 +296,12 @@ key          binding                       description
   (local-set-key (kbd "C-c C-v") 'gl-conf-visit-include)
   (local-set-key (kbd "C-c C-l") 'gl-conf-list-repos)
   (local-set-key (kbd "C-c C-m") 'gl-conf-mark-repo)
-  
+
   ;; Run user hooks.
   (run-hooks 'gl-conf-mode-hook))
 
 
-;; Show the world what we have 
+;; Show the world what we have
 (provide 'gl-conf-mode)
 (provide 'gl-conf-find-next-repo)
 (provide 'gl-conf-find-prev-repo)
